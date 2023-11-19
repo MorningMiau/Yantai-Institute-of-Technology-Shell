d68165af08fbf1a3b4284207cd197a4a47fdd484
MorningMiau <101609781+MorningMiau@users.noreply.github.com>
2023-11-19 12:19:55 +0800

    Add files via upload

---
+++ dns.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+while true
+do
+    clear
+    echo "烟台理工学院 Ubuntu/Debian 一键脚本"
+    echo "Version 0.4 Beta"
+    figlet "YIT"
+    echo "---------------------------------------------------------------"
+    echo "DNS工具"
+    echo "---------------------------------------------------------------"
+    echo "1、查询本机DNS"
+    echo "2、修改本机DNS"
+    echo "---------------------------------------------------------------"
+    echo "0、返回上一级菜单"
+    echo "---------------------------------------------------------------"
+    read -p "请输入数字：" num
+
+    case $num in
+        1)
+            # 查询本机DNS
+            cat /etc/resolv.conf
+            read -p "Press enter to continue"
+            ;;
+        2)
+            # 修改本机DNS
+            echo "DNS 1："
+            read dns1
+            echo "DNS 2："
+            read dns2
+            echo "nameserver $dns1" | sudo tee /etc/resolv.conf > /dev/null
+            echo "nameserver $dns2" | sudo tee -a /etc/resolv.conf > /dev/null
+            ;;
+        0)
+            exit 0
+            ;;
+        *)
+            echo "请输入正确选项"
+            sleep 2
+            ;;
+    esac
+done
